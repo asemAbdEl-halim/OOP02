7884749500fac2102932e63fb110486b662c134e
asemAbdEl-halim <asemabdelhalim78@gmail.com>
2026-08-11 19:51:19 +0300

    Quetion05_Part02

--- Program.cs
+++ Program.cs
@@ -86,7 +86,220 @@
              * **/
 
             #endregion
+
+            #region Quetion05
+            //    // 1. Create DeliveryCenter
+            //    DeliveryCenter center = new DeliveryCenter();
+
+            //    // 2. Read Center Name
+            //    Console.Write("Enter Center Name: ");
+            //    center.CenterName = Console.ReadLine();
+
+            //    Console.WriteLine();
+
+
+            //    // =========================
+            //    // Standard Shipment
+            //    // =========================
+
+            //    Console.WriteLine("Enter Standard Shipment Data");
+
+            //    Console.Write("Tracking Code: ");
+            //    string standardCode = Console.ReadLine();
+
+            //    Console.Write("Description: ");
+            //    string standardDescription = Console.ReadLine();
+
+            //    Console.Write("Weight: ");
+            //    decimal standardWeight = Convert.ToDecimal(Console.ReadLine());
+
+            //    Console.Write("Delivery Fee: ");
+            //    decimal standardFee = Convert.ToDecimal(Console.ReadLine());
+
+            //    Console.Write("City: ");
+            //    string standardCity = Console.ReadLine();
+
+            //    Console.Write("Street: ");
+            //    string standardStreet = Console.ReadLine();
+
+            //    Console.Write("Building Number: ");
+            //    int standardBuilding = Convert.ToInt32(Console.ReadLine());
+
+            //    DeliveryAddress standardAddress =
+            //        new DeliveryAddress(
+            //            standardCity,
+            //            standardStreet,
+            //            standardBuilding);
+
+            //    StandardShipment standardShipment =
+            //        new StandardShipment(
+            //            standardCode,
+            //            standardDescription,
+            //            standardWeight,
+            //            standardFee,
+            //            standardAddress);
+
+            //    center.AddShipment(standardShipment);
+
+            //    Console.WriteLine("Shipment added successfully.");
+            //    Console.WriteLine();
+
+
+            //    // =========================
+            //    // Express Shipment
+            //    // =========================
+
+            //    Console.WriteLine("Enter Express Shipment Data");
+
+            //    Console.Write("Tracking Code: ");
+            //    string expressCode = Console.ReadLine();
+
+            //    Console.Write("Description: ");
+            //    string expressDescription = Console.ReadLine();
+
+            //    Console.Write("Weight: ");
+            //    decimal expressWeight = Convert.ToDecimal(Console.ReadLine());
+
+            //    Console.Write("Delivery Fee: ");
+            //    decimal expressFee = Convert.ToDecimal(Console.ReadLine());
+
+            //    Console.Write("Extra Fee: ");
+            //    decimal extraFee = Convert.ToDecimal(Console.ReadLine());
+
+            //    Console.Write("City: ");
+            //    string expressCity = Console.ReadLine();
+
+            //    Console.Write("Street: ");
+            //    string expressStreet = Console.ReadLine();
+
+            //    Console.Write("Building Number: ");
+            //    int expressBuilding = Convert.ToInt32(Console.ReadLine());
+
+            //    DeliveryAddress expressAddress =
+            //        new DeliveryAddress(
+            //            expressCity,
+            //            expressStreet,
+            //            expressBuilding);
+
+            //    ExpressShipment expressShipment =
+            //        new ExpressShipment(
+            //            expressCode,
+            //            expressDescription,
+            //            expressWeight,
+            //            expressFee,
+            //            expressAddress,
+            //            extraFee);
+
+            //    center.AddShipment(expressShipment);
+
+            //    Console.WriteLine("Shipment added successfully.");
+            //    Console.WriteLine();
+
+
+            //    // =========================
+            //    // International Shipment
+            //    // =========================
+
+            //    Console.WriteLine("Enter International Shipment Data");
+
+            //    Console.Write("Tracking Code: ");
+            //    string internationalCode = Console.ReadLine();
+
+            //    Console.Write("Description: ");
+            //    string internationalDescription = Console.ReadLine();
+
+            //    Console.Write("Weight: ");
+            //    decimal internationalWeight = Convert.ToDecimal(Console.ReadLine());
+
+            //    Console.Write("Delivery Fee: ");
+            //    decimal internationalFee = Convert.ToDecimal(Console.ReadLine());
+
+            //    Console.Write("Destination Country: ");
+            //    string country = Console.ReadLine();
+
+            //    Console.Write("Customs Fee: ");
+            //    decimal customsFee = Convert.ToDecimal(Console.ReadLine());
+
+            //    Console.Write("City: ");
+            //    string internationalCity = Console.ReadLine();
+
+            //    Console.Write("Street: ");
+            //    string internationalStreet = Console.ReadLine();
+
+            //    Console.Write("Building Number: ");
+            //    int internationalBuilding = Convert.ToInt32(Console.ReadLine());
+
+            //    DeliveryAddress internationalAddress =
+            //        new DeliveryAddress(
+            //            internationalCity,
+            //            internationalStreet,
+            //            internationalBuilding);
+
+            //    InternationalShipment internationalShipment =
+            //        new InternationalShipment(
+            //            internationalCode,
+            //            internationalDescription,
+            //            internationalWeight,
+            //            internationalFee,
+            //            internationalAddress,
+            //            country,
+            //            customsFee);
+
+            //    center.AddShipment(internationalShipment);
+
+            //    Console.WriteLine("Shipment added successfully.");
+            //    Console.WriteLine();
+
+
+            //    // 8. Print all shipments
+            //    Console.WriteLine("========== All Shipments ==========");
+            //    center.PrintAllShipments();
+
+
+            //    // 9. Search for a shipment
+            //    Console.WriteLine();
+            //    Console.Write("Enter Tracking Code to Search: ");
+            //    string searchCode = Console.ReadLine();
+
+            //    Shipment foundShipment = center[searchCode];
+
+            //    if (foundShipment != null)
+            //    {
+            //        Console.WriteLine("Shipment Found:");
+            //        foundShipment.PrintShipment();
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Shipment not found.");
+            //    }
+
+
+            //    // 10. Remove shipment
+            //    Console.WriteLine();
+            //    Console.Write("Enter Tracking Code to Remove: ");
+            //    string removeCode = Console.ReadLine();
+
+            //    bool removed = center.RemoveShipment(removeCode);
+
+            //    if (removed)
+            //    {
+            //        Console.WriteLine("Shipment Removed Successfully.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Shipment not found.");
+            //    }
+
+
+            //    // 11. Print remaining shipments
+            //    Console.WriteLine();
+            //    Console.WriteLine("========== Remaining Shipments ==========");
+            //    center.PrintAllShipments();
+            //}
             #endregion
+
+            #endregion
+
         }
     }
 }
