56046bbb451e592e1fad33d5dc258b08309f149c
asemAbdEl-halim <asemabdelhalim78@gmail.com>
2026-08-11 18:10:49 +0300

    Quetion01(b)_Part01

--- Program.cs
+++ Program.cs
@@ -13,7 +13,21 @@
              * A class is a reference type, while a struct is a value type.
              * A class is stored as an object, and a struct is copied when we assign it to another variable.
              * Classes can have more complex behavior, while structs are usually used for small amounts of data.
+             * 
+             * 1- b) Why are classes more suitable than structs for large applications?
+                * ==> Answer <==
+                * Classes are better for large applications because they support inheritance, encapsulation, and polymorphism.
+                * They also make it easier to manage and share objects in large programs.
              ***/
+            #endregion
+
+            #region Quetion02
+
+            /*
+             * a) a) Which class is the parent class?
+                * ==> Answer <==
+             * **/
+
             #endregion
 
             #endregion
