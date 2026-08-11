db9101dd4b915c3d996524a6f2e928a8b8a32ee4
asemAbdEl-halim <asemabdelhalim78@gmail.com>
2026-08-11 18:06:16 +0300

    Quetion01_Part01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part01
+
+            #region Quetion01
+            /*
+             *1- a) What is the difference between a class and a struct?
+             * ==> Answer <==
+             * A class is a reference type, while a struct is a value type.
+             * A class is stored as an object, and a struct is copied when we assign it to another variable.
+             * Classes can have more complex behavior, while structs are usually used for small amounts of data.
+             ***/
+            #endregion
+
+            #endregion
         }
     }
 }
